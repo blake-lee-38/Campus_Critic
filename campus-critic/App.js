bef22ca38fa2af52734344ce032349b023246b5c
Blake Lee <137326318+blake-lee-38@users.noreply.github.com>
2024-04-12 01:36:29 -0500

    Integrating Branches and Fixing Conflicts

--- campus-critic/App.js
+++ campus-critic/App.js
@@ -1,27 +1,40 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import SplashScreen from './components/SplashScreen';
-import LoginScreen from './components/LoginScreen';
-import RegisterScreen from './components/RegisterScreen';
+import SplashScreen from "./components/SplashScreen";
+import LoginScreen from "./components/LoginScreen";
+import RegisterScreen from "./components/RegisterScreen";
+import HomeScreen from "./components/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  console.log("App.js is running!");
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Splash Screen" component={SplashScreen} 
-          options= {{headerShown: false,}}
-          />
-          <Stack.Screen name="Login Screen" component={LoginScreen} 
-          options= {{headerShown: false,}}
-          />
-          <Stack.Screen name="Register Screen" component={RegisterScreen} 
-          options= {{headerShown: false,}}
-          />
+        <Stack.Screen
+          name="Splash Screen"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login Screen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register Screen"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home Screen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- campus-critic/components/RegisterScreen.js
+++ campus-critic/components/RegisterScreen.js
@@ -1,157 +1,176 @@
-import * as React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import colors from '../assets/colors/colors'; // Assuming you have color definitions here
-import { useNavigation } from '@react-navigation/native'; // Import useNavigation
+import * as React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import colors from "../assets/colors/colors"; // Assuming you have color definitions here
+import { useNavigation } from "@react-navigation/native"; // Import useNavigation
+import { signUp, signUpGoogle } from "../methods/auth";
 
 export default function LoginScreen() {
-    const navigation = useNavigation();
-    const [username, setUsername] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [confirmPassword, setConfirmPassword] = React.useState('');
+  const navigation = useNavigation();
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
 
-    const handleRegister = () => {
-        // Implement your register functionality here
-        console.log('Register button pressed');
-    };
+  const handleRegister = async () => {
+    // Implement your register functionality here
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwords do not match");
+      return;
+    }
+    const signUpResult = await signUp(email, password);
+    if (signUpResult.message === "Success") {
+      console.log("User created!", signUpResult.user);
+      navigation.navigate("Home Screen");
+    } else {
+      setErrorMessage(signUpResult.message);
+      console.log(signUpResult.message);
+    }
+  };
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                style={styles.backButton}
-                onPress={() => navigation.navigate('Splash Screen')}
-            >
-                <Text style={styles.backButtonText}>Back</Text>
-            </TouchableOpacity>
-            <Text style={styles.welcomeBack}>Hello! Register to get started.</Text>
-            <View style={styles.inputField}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Enter your username"
-                    placeholderTextColor={colors.gray}
-                    value={username}
-                    onChangeText={setUsername}
-                />
-            </View>
-            <View style={styles.inputField}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Enter your email"
-                    placeholderTextColor={colors.gray}
-                    keyboardType="email-address"
-                    value={email}
-                    onChangeText={setEmail}
-                />
-            </View>
-            <View style={styles.inputField}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Enter your password"
-                    placeholderTextColor={colors.gray}
-                    secureTextEntry={true}
-                    value={password}
-                    onChangeText={setPassword}
-                />
-            </View>
-            <View style={styles.inputField}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Confirm your password"
-                    placeholderTextColor={colors.gray}
-                    secureTextEntry={true}
-                    value={confirmPassword}
-                    onChangeText={setConfirmPassword}
-                />
-            </View>
-            <TouchableOpacity
-                style={styles.rectangle173}
-                onPress={handleRegister}
-            >
-                <Text style={styles.register}>Register</Text>
-            </TouchableOpacity>
-            <View style={styles.root}>
-                <Text style={styles.alreadyHaveAnAccount}>Already have an account? </Text>
-                <TouchableOpacity onPress={() => navigation.navigate('Login Screen')}>
-                    <Text style={[styles.loginNow, styles.underline]}>Login now!</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.navigate("Splash Screen")}
+      >
+        <Text style={styles.backButtonText}>Back</Text>
+      </TouchableOpacity>
+      <Text style={styles.welcomeBack}>Hello! Register to get started.</Text>
+      <View style={styles.inputField}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter your username"
+          placeholderTextColor={colors.gray}
+          value={username}
+          onChangeText={setUsername}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter your email"
+          placeholderTextColor={colors.gray}
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter your password"
+          placeholderTextColor={colors.gray}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      <View style={styles.inputField}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Confirm your password"
+          placeholderTextColor={colors.gray}
+          secureTextEntry={true}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+      </View>
+      <Text style={styles.underline}>{errorMessage}</Text>
+      <TouchableOpacity style={styles.rectangle173} onPress={handleRegister}>
+        <Text style={styles.register}>Register</Text>
+      </TouchableOpacity>
+      <View style={styles.root}>
+        <Text style={styles.alreadyHaveAnAccount}>
+          Already have an account?{" "}
+        </Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Login Screen")}>
+          <Text style={[styles.loginNow, styles.underline]}>Login now!</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.background,
-    },
-    backButton: {
-        position: 'absolute',
-        top: 50,
-        left: 15,
-        padding: 10,
-    },
-    backButtonText: {
-        color: colors.primary,
-    },
-    welcomeBack: {
-        color: colors.primary,
-        fontSize: 30,
-        fontWeight: '700',
-        marginBottom: 20,
-        alignSelf: 'flex-start',
-        marginLeft: 35,
-    },
-    inputField: {
-        width: 331,
-        height: 56,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderColor: colors.border,
-        backgroundColor: 'rgba(247, 248, 249, 1)',
-        marginBottom: 10,
-        justifyContent: 'center',
-    },
-    inputText: {
-        color: colors.gray,
-        fontSize: 15,
-        paddingHorizontal: 10,
-    },
-    rectangle173: {
-        width: 331,
-        height: 56,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderColor: colors.primary,
-        backgroundColor: colors.primary,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    register: {
-        color: colors.white,
-        textAlign: 'center',
-        fontSize: 15,
-        fontWeight: '600',
-    },
-    root: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 50,
-    },
-    alreadyHaveAnAccount: {
-        color: colors.primary,
-        fontSize: 15,
-        fontWeight: '700',
-    },
-    loginNow: {
-        color: colors.secondary,
-        fontSize: 15,
-        fontWeight: '600',
-    },
-    underline: {
-        textDecorationLine: 'underline',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.background,
+  },
+  backButton: {
+    position: "absolute",
+    top: 50,
+    left: 15,
+    padding: 10,
+  },
+  backButtonText: {
+    color: colors.primary,
+  },
+  welcomeBack: {
+    color: colors.primary,
+    fontSize: 30,
+    fontWeight: "700",
+    marginBottom: 20,
+    alignSelf: "flex-start",
+    marginLeft: 35,
+  },
+  inputField: {
+    width: 331,
+    height: 56,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: colors.border,
+    backgroundColor: "rgba(247, 248, 249, 1)",
+    marginBottom: 10,
+    justifyContent: "center",
+  },
+  inputText: {
+    color: colors.gray,
+    fontSize: 15,
+    paddingHorizontal: 10,
+  },
+  rectangle173: {
+    width: 331,
+    height: 56,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: colors.primary,
+    backgroundColor: colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  register: {
+    color: colors.white,
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  root: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  alreadyHaveAnAccount: {
+    color: colors.primary,
+    fontSize: 15,
+    fontWeight: "700",
+  },
+  loginNow: {
+    color: colors.secondary,
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  underline: {
+    textDecorationLine: "underline",
+  },
 });
