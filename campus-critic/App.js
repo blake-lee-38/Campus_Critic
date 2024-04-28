b20a093e3acf7876f52ab0bbde60135a9aaa7f58
ariannatobias <123114273+ariannatobias@users.noreply.github.com>
2024-04-28 15:37:10 -0500

    Added a bottom tab navigator
    Idk if we want to use it the way I set it up but I went ahead and made it just in case.

--- campus-critic/App.js
+++ campus-critic/App.js
@@ -1,14 +1,54 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { SafeAreaView, ScrollView, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import SplashScreen from "./components/SplashScreen";
 import LoginScreen from "./components/LoginScreen";
 import RegisterScreen from "./components/RegisterScreen";
 import HomeScreen from "./components/HomeScreen";
+import colors from "../campus-critic/assets/colors/colors.js";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Tab Navigator
+function TabScreen() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'AddReview') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={40} color={colors.primary} />;
+        },
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.placeholderGray,
+        tabBarStyle: {
+          backgroundColor: 'white',
+          paddingBottom: 25, 
+          paddingVertical: 15, 
+          height: 80,
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} /> 
+      {/* Add the AddReviewScreen component */}
+      {/* Add the ProfileScreen component */}
+    </Tab.Navigator>
+  );
+}
 
 function App() {
   return (
@@ -29,9 +69,10 @@ function App() {
           component={RegisterScreen}
           options={{ headerShown: false }}
         />
+        {/* Use the TabScreen as a stack screen */}
         <Stack.Screen
           name="Home Screen"
-          component={HomeScreen}
+          component={TabScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
