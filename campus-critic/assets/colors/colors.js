31a4722f2a08a3f75d21a949c8b48e0179c3577b
ariannatobias <123114273+ariannatobias@users.noreply.github.com>
2024-04-28 20:13:09 -0500

    Home page finished + Added a tab navigator (#13)
    * Updated colors.js
    
    * Changed titles
    
    * updated homeScreen
    
    * category scroll works
    
    * recent reviews scroll
    
    * Recent reviews working
    
    * Home Screen is working
    
    * Installed tab navigator pkg
    
    * Added a bottom tab navigator
    
    Idk if we want to use it the way I set it up but I went ahead and made it just in case.

--- campus-critic/assets/colors/colors.js
+++ campus-critic/assets/colors/colors.js
@@ -6,7 +6,10 @@ const colors = {
     border: '#E8ECF4',
     gray: '#8391A1',    
     darkGray: '#6A707C',
-
+    star: '#FFCB45',
+    placeholderGray: '#EEEEEE',
+    textBack: '#F6F6F6',
+    open: '#008405',
 }
 
 export default colors;
