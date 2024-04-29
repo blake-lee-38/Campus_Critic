d7d0573587a81c0fb3d9fb1ce63836a0ff15579c
Blake Lee <137326318+blake-lee-38@users.noreply.github.com>
2024-04-28 20:20:57 -0500

    Register Screen Changes

--- campus-critic/components/RegisterScreen.js
+++ campus-critic/components/RegisterScreen.js
@@ -8,12 +8,14 @@ import {
   ActivityIndicator,
   KeyboardAvoidingView,
   ScrollView,
+  SafeAreaView,
 } from "react-native";
 import colors from "../assets/colors/colors"; // Assuming you have color definitions here
 import { useNavigation } from "@react-navigation/native"; // Import useNavigation
 import { signUp, signUpGoogle } from "../methods/auth";
 import DropDownPicker from "react-native-dropdown-picker";
 import { storeUserData } from "../methods/dbMethods";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function LoginScreen() {
   const navigation = useNavigation();
@@ -85,107 +87,133 @@ export default function LoginScreen() {
   };
 
   return (
-    <ScrollView>
-      <KeyboardAvoidingView style={styles.container}>
-        <TouchableOpacity
-          style={styles.backButton}
-          onPress={() => navigation.navigate("Splash Screen")}
-        >
-          <Text style={styles.backButtonText}>Back</Text>
-        </TouchableOpacity>
-        <Text style={styles.welcomeBack}>Hello! Register to get started.</Text>
-        <Text style={styles.category}>Personal Information</Text>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Enter your First Name"
-            placeholderTextColor={colors.gray}
-            value={firstName}
-            onChangeText={setFirstName}
-          />
-        </View>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Enter your Last Name"
-            placeholderTextColor={colors.gray}
-            value={lastName}
-            onChangeText={setLastName}
-          />
-        </View>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Enter your username"
-            placeholderTextColor={colors.gray}
-            value={username}
-            onChangeText={setUsername}
-          />
-        </View>
-        <Text style={styles.category}>Account Sign-In Information</Text>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Enter your email"
-            placeholderTextColor={colors.gray}
-            keyboardType="email-address"
-            value={email}
-            onChangeText={setEmail}
-          />
-        </View>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Enter your password"
-            placeholderTextColor={colors.gray}
-            secureTextEntry={true}
-            value={password}
-            onChangeText={setPassword}
-          />
-        </View>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.inputText}
-            placeholder="Confirm your password"
-            placeholderTextColor={colors.gray}
-            secureTextEntry={true}
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-          />
-        </View>
-        <Text style={styles.category}>Choose Your Home College!</Text>
-        <DropDownPicker
-          open={open}
-          value={college}
-          items={items}
-          setOpen={setOpen}
-          setValue={setCollege}
-          setItems={setItems}
-          style={styles.inputField}
-          containerStyle={{ width: 331, height: 56 }}
-          textStyle={styles.inputText}
-        />
-        <Text style={styles.underline}>{errorMessage}</Text>
-        {loading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
-        ) : (
-          <TouchableOpacity
-            style={styles.rectangle173}
-            onPress={handleRegister}
-          >
-            <Text style={styles.register}>Register</Text>
-          </TouchableOpacity>
-        )}
-        <View style={styles.root}>
-          <Text style={styles.alreadyHaveAnAccount}>
-            Already have an account?{" "}
-          </Text>
-          <TouchableOpacity onPress={() => navigation.navigate("Login Screen")}>
-            <Text style={[styles.loginNow, styles.underline]}>Login now!</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
-    </ScrollView>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <ScrollView>
+          <KeyboardAvoidingView style={styles.container}>
+            <View
+              style={{
+                flexDirection: "row",
+                flex: 1,
+                padding: 10,
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={styles.backButton}
+              >
+                <View>
+                  <Ionicons
+                    name="arrow-back-circle-outline"
+                    size={24}
+                    color="white"
+                  />
+                </View>
+              </TouchableOpacity>
+              <View style={styles.title}>
+                <Text style={styles.welcomeBack}>
+                  Hello! Register to get started.
+                </Text>
+              </View>
+            </View>
+            <Text style={styles.category}>Personal Information</Text>
+            <View style={styles.inputField}>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Enter your First Name"
+                placeholderTextColor={colors.gray}
+                value={firstName}
+                onChangeText={setFirstName}
+              />
+            </View>
+            <View style={styles.inputField}>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Enter your Last Name"
+                placeholderTextColor={colors.gray}
+                value={lastName}
+                onChangeText={setLastName}
+              />
+            </View>
+            <View style={styles.inputField}>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Enter your username"
+                placeholderTextColor={colors.gray}
+                value={username}
+                onChangeText={setUsername}
+              />
+            </View>
+            <Text style={styles.category}>Account Sign-In Information</Text>
+            <View style={styles.inputField}>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Enter your email"
+                placeholderTextColor={colors.gray}
+                keyboardType="email-address"
+                value={email}
+                onChangeText={setEmail}
+              />
+            </View>
+            <View style={styles.inputField}>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Enter your password"
+                placeholderTextColor={colors.gray}
+                secureTextEntry={true}
+                value={password}
+                onChangeText={setPassword}
+              />
+            </View>
+            <View style={styles.inputField}>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Confirm your password"
+                placeholderTextColor={colors.gray}
+                secureTextEntry={true}
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+              />
+            </View>
+            <Text style={styles.category}>Choose Your Home College!</Text>
+            <DropDownPicker
+              open={open}
+              value={college}
+              items={items}
+              setOpen={setOpen}
+              setValue={setCollege}
+              setItems={setItems}
+              style={styles.inputField}
+              containerStyle={{ width: 331, height: 56 }}
+              textStyle={styles.inputText}
+            />
+            <Text style={styles.underline}>{errorMessage}</Text>
+            {loading ? (
+              <ActivityIndicator size="large" color="#0000ff" />
+            ) : (
+              <TouchableOpacity
+                style={styles.rectangle173}
+                onPress={handleRegister}
+              >
+                <Text style={styles.register}>Register</Text>
+              </TouchableOpacity>
+            )}
+            <View style={styles.root}>
+              <Text style={styles.alreadyHaveAnAccount}>
+                Already have an account?{" "}
+              </Text>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Login Screen")}
+              >
+                <Text style={[styles.loginNow, styles.underline]}>
+                  Login now!
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 }
 
@@ -197,28 +225,38 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
   },
   backButton: {
-    position: "absolute",
-    top: 50,
-    left: 15,
-    padding: 10,
-  },
-  backButtonText: {
-    color: colors.primary,
+    backgroundColor: colors.primary,
+    height: 56,
+    width: "15%",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    marginRight: 10,
   },
   welcomeBack: {
-    color: colors.primary,
-    fontSize: 30,
+    color: colors.white,
+    fontSize: 18,
     fontWeight: "700",
-    marginBottom: 20,
-    marginLeft: 35,
   },
   category: {
     color: colors.primary,
     fontSize: 14,
     fontWeight: "700",
-    marginBottom: 20,
+    marginVertical: 20,
     marginLeft: 35,
     justifyContent: "center",
+  },
+  title: {
+    width: "80%",
+
+    height: 56,
+    borderRadius: 8,
+    backgroundColor: colors.primary,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
   },
   inputField: {
     width: 331,
